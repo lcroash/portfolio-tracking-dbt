be989e440df6b3310ad13d9359b5c7a663f736e0
Liam Croash <lcroash@gmail.com>
2024-02-06 18:29:35 +0000

    refactor

--- models/refined/POSITON_ABC_BANK.sql
+++ models/refined/POSITON_ABC_BANK.sql
@@ -3,4 +3,5 @@ SELECT
     ,POSITION_VALUE - COST_BASE as UNREALIZED_PROFIT
     ,ROUND(UNREALIZED_PROFIT / COST_BASE, 5) as UNREALIZED_PROFIT_PCT
 FROM
-    {{ source('abc_bank', 'ABC_BANK_POSITION')}}
+    -- {{ source('abc_bank', 'ABC_BANK_POSITION')}}
+    {{ ref('STG_ABC_BANK_POSITION') }}
