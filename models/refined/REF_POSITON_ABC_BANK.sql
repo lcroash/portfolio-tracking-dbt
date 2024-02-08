8a1f8b4237e1c7a779f63245c37f9671b367d569
Liam Croash <lcroash@gmail.com>
2024-02-08 20:29:19 +0000

    switching ref to use snapshot rather than stging

--- models/refined/REF_POSITON_ABC_BANK.sql
+++ models/refined/REF_POSITON_ABC_BANK.sql
@@ -1,7 +1,14 @@
+WITH current_from_snapshot as (
+    SELECT *
+    FROM {{ ref('SNSH_ABC_BANK_POSITION') }}
+    WHERE DBT_VALID IS NULL
+)
+
 SELECT 
     *
     ,POSITION_VALUE - COST_BASE as UNREALIZED_PROFIT
     ,ROUND(UNREALIZED_PROFIT / COST_BASE, 5) as UNREALIZED_PROFIT_PCT
 FROM
     -- {{ source('abc_bank', 'ABC_BANK_POSITION')}}
-    {{ ref('STG_ABC_BANK_POSITION') }}
+    -- {{ ref('STG_ABC_BANK_POSITION') }}
+    current_from_snapshot
